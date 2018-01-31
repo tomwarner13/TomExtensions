5ec49fcedadc322883b4b3c54e3d88abab485ae8
tomwarner13 <tomwarner13@gmail.com>
2018-01-31 10:15:53 -0500

    fix whitespace, none<func> should call none<predicate>

--- TomExtensions/Collections/EnumerableExtensions.cs
+++ TomExtensions/Collections/EnumerableExtensions.cs
@@ -7,28 +7,28 @@ using System.Text;
 
 namespace TomExtensions.Collections
 {
-    public static class EnumerableExtensions
-    {
-      public static bool None<T>(this IEnumerable<T> self) => !self.Any();
+  public static class EnumerableExtensions
+  {
+    public static bool None<T>(this IEnumerable<T> self) => !self.Any();
 
-      public static bool None<T>(this IEnumerable<T> self, Predicate<T> predicate) => 
-        !self.Any(predicate);
+    public static bool None<T>(this IEnumerable<T> self, Predicate<T> predicate) =>
+      !self.Any(predicate);
 
-      public static bool None<T>(this IEnumerable<T> self, Func<T, bool> func) =>
-        !self.Any(new Predicate<T>(func));
+    public static bool None<T>(this IEnumerable<T> self, Func<T, bool> func) =>
+      self.None(new Predicate<T>(func));
 
     public static bool Any<T>(this IEnumerable<T> self, Predicate<T> predicate) =>
         self.Any((Func<T, bool>)(e => predicate(e)));
 
-      public static IEnumerable<int> Range(int count) => 
-        Enumerable.Range(0, count);
-    
-      public static IEnumerable<T> Generate<T>(Func<T> generator, int count)
+    public static IEnumerable<int> Range(int count) =>
+      Enumerable.Range(0, count);
+
+    public static IEnumerable<T> Generate<T>(Func<T> generator, int count)
+    {
+      for (var i = 0; i < count; i++)
       {
-        for (var i = 0; i < count; i++)
-        {
-          yield return generator();
-        }
+        yield return generator();
       }
     }
+  }
 }
